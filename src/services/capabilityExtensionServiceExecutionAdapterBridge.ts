ebe7b919bf7701a0bb97afaf35f78d9933754ee5
temeaco-max <temea.co@gmail.com>
2026-08-17 23:12:37 +0100

    Expose executable extension adapter bridge

---
+++ src/services/capabilityExtensionServiceExecutionAdapterBridge.ts
@@ -0,0 +1,2 @@
+// Compatibility export for extension adapter consumers.
+export { getCapabilityExecutionAdapter, listCapabilityExecutionAdapters, registerCapabilityExecutionAdapter } from './capabilityExtensionService.js';
