ed423a5100f68da06726d2a728c106a902dfbddd
temeaco-max <temea.co@gmail.com>
2026-08-17 23:11:29 +0100

    Bridge execution adapters into canonical executor

---
+++ src/services/capabilityExecutionAdapterBridge.ts
@@ -0,0 +1,5 @@
+import { getCapabilityExecutionAdapter, listCapabilityExecutionAdapters, registerCapabilityExecutionAdapter } from './capabilityExtensionService.js';
+
+export const getExecutionAdapter = getCapabilityExecutionAdapter;
+export const listExecutionAdapters = listCapabilityExecutionAdapters;
+export const registerExecutionAdapter = registerCapabilityExecutionAdapter;
