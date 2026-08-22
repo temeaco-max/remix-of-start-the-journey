5f775ecf8f440ed621efb2a062a32fcf4f07a616
temeaco-max <2.60085079e+08+temeaco-max@users.noreply.github.com>
2026-08-22 14:37:36 +0000

    Align Chat message action targets

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,8 @@
-const STATIC_CACHE = 'kurukoo-static-v10';
+const STATIC_CACHE = 'kurukoo-static-v11';
 const PAGES_CACHE = 'kurukoo-pages-v10';
 const PWA_SHELL_CACHE = 'kurukoo-pwa-shell-v10';
 const ALLOWED_CACHES = [STATIC_CACHE, PAGES_CACHE, PWA_SHELL_CACHE];
-const LIVE_STATIC_PATHS = new Set(['/css/site.css', '/css/kurukoo-chat.css', '/css/kurukoo-workspace.css', '/js/kurukoo-primary-chat.js', '/js/kurukoo-workspace.js', '/sw.js']);
+const LIVE_STATIC_PATHS = new Set(['/css/site.css', '/css/kurukoo-chat.css', '/css/kurukoo-workspace.css', '/css/kurukoo-chat-visual-completion.css', '/js/kurukoo-primary-chat.js', '/js/kurukoo-workspace.js', '/js/kurukoo-pwa.js', '/sw.js']);
 
 const SHELL_ASSETS = [
     '/chat/',
